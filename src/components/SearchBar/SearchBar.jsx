400165a54c8acf2800c4b280511118fa25ae8e34
veronika <artemova331@gmail.com>
2025-05-14 20:59:27 +0300

    add components

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SearchBar = () => {
+  return (
+    <header>
+      <form>
+        <input type="text" autocomplete="off" autofocus placeholder="Search images and photos" />
+        <button type="submit">Search</button>
+      </form>
+    </header>
+  );
+};
+
+export default SearchBar;
